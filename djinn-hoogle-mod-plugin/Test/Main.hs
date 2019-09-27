69710f9993344c33f2a7f992c8860c71d65cb9f8
Matthías Páll Gissurarson <pallm@chalmers.se>
2019-09-27 16:36:37 +0200

    revert changes to hole name plugin

--- djinn-hoogle-mod-plugin/Test/Main.hs
+++ djinn-hoogle-mod-plugin/Test/Main.hs
@@ -1,8 +1,16 @@
-{-# OPTIONS -fplugin=DjinnHoogleModPlugin -funclutter-valid-hole-fits #-}
+{-# OPTIONS -fplugin=DjinnHoogleModPlugin
+            -funclutter-valid-hole-fits #-}
 module Main where
 import Control.Monad
+f :: (a,b,c) -> a
+f = _invoke_Djinn
+f2 :: (a,b,c) -> a
+f2 = _invoke_Hoogle
+g :: [a] -> [[a]]
+g = _invoke_Hoogle
+h :: [[a]] -> [a]
+h = _module_Control_Monad
 
-f :: (a,b) -> b
-f = _invoke_Hoogle
 
+main :: IO ()
 main = return ()
